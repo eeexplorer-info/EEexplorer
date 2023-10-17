40763d5f4d57a5825fb579171cd827a9cc09c362
rob-smi <138279537+rob-smi@users.noreply.github.com>
2023-10-17 14:01:06 +1100

    Test: Visitor Counter

--- index.js
+++ index.js
@@ -9,14 +9,14 @@ if (visitCount) {
   visitCount = Number(visitCount) + 1;
   localStorage.setItem("page_view", visitCount);
 } else {
-  visitCount = 100;
+  visitCount = 1;
   localStorage.setItem("page_view", 1);
 }
 counterContainer.innerHTML = visitCount;
 
 // Adding onClick event listener
 resetButton.addEventListener("click", () => {
-  visitCount = 100;
+  visitCount = 1;
   localStorage.setItem("page_view", 1);
   counterContainer.innerHTML = visitCount;
 });
