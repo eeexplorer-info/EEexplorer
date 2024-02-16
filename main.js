08b4c4c064d86e3ff1af97eb3ee3cc67d43be19e
rob-smi <138279537+rob-smi@users.noreply.github.com>
2024-02-16 20:29:28 +1100

    Quicker choices by untangling table options
    ------=== 16 Feb 2024 ===------
    {
    == ABOUT
    + Navigation instructions untangled
    
    == ALUMNI Activity
    + Ctee Honour Board created & populated
    + Tables untangled & unified
    
    == CHARTS & TABLES
    + Discrete component quick lookup added
    + Resistor E-Series 3, 6, 12, 24, 96 added
    + Telecommunication twisted pairs added
    + same, Cat 1-8 cable definitions added
    + Tables: Chemistry, Computer Data unified
    + same: Connectors, Human Communication,
    + same: Logic Gates, Mathematics, Physics,
    + same: Telecom, Three Phase
    
    == ELSOC Activity
    + Tables untangled & unified
    
    == WORK n LIFE
    + Fortune cookie pop-up & down mechanism created
    }

---
+++ main.js
@@ -0,0 +1,5 @@
+// 240211 - Generate Random Number between 12 and 100
+// It works in the HTML <script></script> tags
+function random(){
+    document.getElementById("one").innerHTML = Math.floor(Math.random() * 100)+1;
+    }
