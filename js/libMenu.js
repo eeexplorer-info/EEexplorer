5140aaa0972c0fc0fe07ea2e7b3610c8b411343d
rob-smi <138279537+rob-smi@users.noreply.github.com>
2026-04-20 10:55:22 +1000

    Partial Upload of Alumni Post 260422
    ------=== 22 Apr 2026 ===------
    {
    == ALUMNI > POSTS
    - Post 260422 - ANZAC Day -- EMU Robotics Results -- Workplace Quotes
    - Index of Posts updated
    }

--- js/libMenu.js
+++ js/libMenu.js
@@ -1,7 +1,7 @@
 // https://www.w3schools.com/howto/howto_js_topnav_responsive.asp
 // 260204
 
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+// Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon
 function myMenu() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
@@ -33,12 +33,41 @@ function myMenu() {
 // 260204
 //
 // function myMenu() {
-//   var x = document.getElementById("myLinks");
-//   if (x.style.display === "block") {
-    // x.style.display = "none";
-//   } else {
-    // x.style.display = "block";
-//   }
-// }
+//    var x = document.getElementById("myLinks");
+//    if (x.style.display === "block") {
+//      x.style.display = "none";
+//    } else {
+//      x.style.display = "block";
+//    }
+//  }
 //</script>
 
+
+// https://www.w3schools.com/js/js_popup.asp
+// 260209
+// function myFnAlert() {
+  // alert("An alert box!");
+  // ALUMNI\nConnect Nights; Event Calendar; Feedback Paths; Honour Boards; Meetings; Publications; Site Tours; Social Network; Social Posts\n\n
+  // ELSOC\nCampus Visits; Contacts; Event Calendar; Honour Board; Publications; Social Graphics; Social Network; Term Calendar\n\n
+  // SCHOOL\nContacts; Event Calendar; RElated; Term Calendar\n\n");
+// }
+
+
+// function myFnConfirm() {
+//   if (confirm("Press a button!")) {
+//     txt = "You pressed OK!";
+//   } else {
+//     txt = "You pressed Cancel!";
+//   } 
+// }
+
+
+// function myFnPrompt() {
+//   let person = prompt("Please enter your name", "Harry Potter");
+// let text;
+// if (person == null || person == "") {
+//   text = "User cancelled the prompt.";
+// } else {
+//   text = "Hello " + person + "! How are you today?";
+// } 
+// }
