9e43e50f9b535c77a142de5a18bfcb22a586caf0
rob-smi <138279537+rob-smi@users.noreply.github.com>
2026-06-01 12:17:21 +1000

    Corrected typo in Menu code

--- js/libMenu.js
+++ js/libMenu.js
@@ -21,7 +21,7 @@ function myMenu() {
 //  document.getElementById("myNav").style.width = "100%";
 //}
 
-/* Close when someone clicks on the "x" symbol inside the overlay */s
+/* Close when someone clicks on the "x" symbol inside the overlay */
 //function closeNav() {
 //  document.getElementById("myNav").style.width = "0%";
 //} 
